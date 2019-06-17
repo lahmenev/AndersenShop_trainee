a3a7fa37f8af01a785b5ed55e43c012f62894be6
lahmenev <ser.lakhmenev1993@mail.ru>
2019-06-18 00:08:43 +0300

    task_5 correct conflict

---
+++ src/main/java/task_4/shop/model/strategies/PaymentByDefault.java
@@ -0,0 +1,22 @@
+package task_4.shop.model.strategies;
+
+/**
+ * email : s.lakhmenev@andersenlab.com
+ *
+ * @author Lakhmenev Sergey
+ * @version 1.1
+ */
+public class PaymentByDefault implements CurrencyStrategy {
+
+    /**
+     * Counts price for products by default case
+     *
+     * @param price argument of price
+     * @param amount argument of product's amount
+     * @return value of final price
+     */
+    @Override
+    public int payment(int price, int amount) {
+        return price * amount;
+    }
+}
